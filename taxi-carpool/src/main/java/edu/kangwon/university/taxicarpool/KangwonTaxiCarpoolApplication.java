75c0b54e0ff57ca44bcb4d7d34d851b792713224
treasure-sky <ych0486666@gmail.com>
2025-03-10 16:47:45 +0900

    chore: google java style guilde 적용
    tab 문자를 공백 4칸으로 변경

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplication.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KangwonTaxiCarpoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KangwonTaxiCarpoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KangwonTaxiCarpoolApplication.class, args);
+    }
 
 }
