85a7f60e20fed39d68c31774e341deea99913d3e
suno-boy <tnsgh3691@gmail.com>
2025-03-21 15:57:43 +0900

    feat: MemberEntity에 연관관계 매핑
    - partyEntity랑 매핑이 안 되어있어서(아직 PR을 합치지 않았기 때문) 테스트가 안 됨. 고로 임의로 만들었음.

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/member/MemberEntity.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/member/MemberEntity.java
@@ -1,12 +1,19 @@
 package edu.kangwon.university.taxicarpool.member;
 
+import edu.kangwon.university.taxicarpool.party.PartyEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity(name = "member")
 public class MemberEntity {
     @Id
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "party_id")
+    private PartyEntity partyEntity;
 
     public void setId(Long id) {
         this.id = id;
