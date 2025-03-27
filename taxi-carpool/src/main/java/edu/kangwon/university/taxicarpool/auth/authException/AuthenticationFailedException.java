1298bc37a643de3106f9e035ceb85c41d132b82c
suno-boy <tnsgh3691@gmail.com>
2025-03-27 15:19:05 +0900

    feat: Auth에서 토큰 제외한 예외처리를 커스텀 예외처리로 바꾸기 위해 커스텀 예외처리 클래스 작성.

---
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/auth/authException/AuthenticationFailedException.java
@@ -0,0 +1,10 @@
+package edu.kangwon.university.taxicarpool.auth.authException;
+
+public class AuthenticationFailedException extends RuntimeException {
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+    public AuthenticationFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
