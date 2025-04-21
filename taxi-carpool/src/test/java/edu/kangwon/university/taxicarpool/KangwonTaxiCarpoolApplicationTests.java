fe33d2ca528f26e77fcf92180616dde8794e206b
suno-boy <tnsgh3691@gmail.com>
2025-04-21 16:06:09 +0900

    refactor: 빌드 과정중의 test를 위한 H2 DB살려두기와 관련 설정

--- taxi-carpool/src/test/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplicationTests.java
+++ taxi-carpool/src/test/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplicationTests.java
@@ -1,11 +1,9 @@
 package edu.kangwon.university.taxicarpool;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class KangwonTaxiCarpoolApplicationTests {
 
 	@Test
