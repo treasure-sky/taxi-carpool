b24c678906c042cb7fcd7a40b4664902adaba573
suno-boy <tnsgh3691@gmail.com>
2025-04-21 17:21:53 +0900

    chore: DB를 환경에 따라 분리

--- taxi-carpool/src/test/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplicationTests.java
+++ taxi-carpool/src/test/java/edu/kangwon/university/taxicarpool/KangwonTaxiCarpoolApplicationTests.java
@@ -3,11 +3,11 @@ package edu.kangwon.university.taxicarpool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = KangwonTaxiCarpoolApplicationTests.class)
 class KangwonTaxiCarpoolApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
