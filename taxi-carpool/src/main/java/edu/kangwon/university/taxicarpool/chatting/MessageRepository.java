78e0995809921520df60386cfcecafe6ec1308f6
treasure-sky <ych0486666@gmail.com>
2025-05-09 21:37:04 +0900

    feat(chat): chatting기능 구현에 필요한 JPA Repository 및 Mapper 추가

---
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/chatting/MessageRepository.java
@@ -0,0 +1,8 @@
+package edu.kangwon.university.taxicarpool.chatting;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+
+}
+
