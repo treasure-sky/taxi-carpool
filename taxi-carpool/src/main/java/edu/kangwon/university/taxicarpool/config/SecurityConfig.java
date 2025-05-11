b07c36ed965d6a6bbc300da4f10f114e42b74976
suno-boy <tnsgh3691@gmail.com>
2025-05-11 22:46:07 +0900

    refactor: 모든 토큰 검증 허용
    - 테스트를 위해 모든 토큰 검증 허용시키기

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/config/SecurityConfig.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/config/SecurityConfig.java
@@ -45,23 +45,24 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) // 필요에 따라 설정해야한다고 함.
             .authorizeHttpRequests(auth -> auth
                 // 회원가입/로그인 엔드포인트는 인증 없이 접근 가능하게
-                .requestMatchers(
-                    "/",                        // 루트
-                    "/api/auth/**",             // 회원가입, 로그인
-                    "/api/email/**",            // 이메일 인증
-                    "/swagger-ui.html",
-                    "/swagger-ui/**",           // 스웨거 UI리소스
-                    "/api-docs/**",          // 스웨거 API 문서
-                    "/swagger-resources/**",    // 스웨거 리소스
-                    "/webjars/**"               // 스웨거 관련 정적 리소스
-                ).permitAll()
+//                .requestMatchers(
+//                    "/",                        // 루트
+//                    "/api/auth/**",             // 회원가입, 로그인
+//                    "/api/email/**",            // 이메일 인증
+//                    "/swagger-ui.html",
+//                    "/swagger-ui/**",           // 스웨거 UI리소스
+//                    "/api-docs/**",          // 스웨거 API 문서
+//                    "/swagger-resources/**",    // 스웨거 리소스
+//                    "/webjars/**"               // 스웨거 관련 정적 리소스
+//                )
+                    .anyRequest().permitAll()
                 // 그 외 모든 요청은 인증 필요
-                .anyRequest().authenticated()
-            )
+//                .anyRequest().authenticated()
+            );
             // JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
             // 난 수동으로 회원가입 DB접근으로 검증해서 UsernamePasswordAuthenticationFilter 안 쓰이긴 함.
             // 나중에 .formLogin().disable()로 막아두는 것 고민하기.
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            // .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
