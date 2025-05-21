dfbe59bdd2fcbc3876585dc6eecba2c259718d72
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2025-05-21 21:06:42 +0900

    fix: userId가 세션에 저장되지 않는 문제 수정

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/chatting/JwtStompInterceptor.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/chatting/JwtStompInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 
@@ -45,7 +46,7 @@ public class JwtStompInterceptor implements ChannelInterceptor {
             }
         }
 
-        return message;
+        return MessageBuilder.createMessage(message.getPayload(), accessor.getMessageHeaders());
     }
 
 }
