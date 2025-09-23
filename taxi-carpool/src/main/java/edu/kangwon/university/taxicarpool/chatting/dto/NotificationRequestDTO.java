8e999a1a1b419f485340c951c92e308f3ea81b61
suno-boy <tnsgh3691@gmail.com>
2025-09-23 18:19:49 +0900

    refactor: 채팅 Entity, DTO 롬복 적용

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/chatting/dto/NotificationRequestDTO.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/chatting/dto/NotificationRequestDTO.java
@@ -1,21 +1,13 @@
 package edu.kangwon.university.taxicarpool.chatting.dto;
 
 import edu.kangwon.university.taxicarpool.profanity.NoProfanity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class NotificationRequestDTO {
 
     @NoProfanity(message = "공지사항에 비속어가 포함되어 있습니다.")
     private String notification;
-
-    public NotificationRequestDTO() {
-    }
-
-    public String getNotification() {
-        return notification;
-    }
-
-    public void setNotification(String notification) {
-        this.notification = notification;
-    }
-
 }
