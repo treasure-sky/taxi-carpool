64417b800a1c7818de8e3023bba337de12e08877
suno-boy <tnsgh3691@gmail.com>
2025-09-23 18:13:04 +0900

    refactor: 예외처리 DTO 롬복 적용

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/exception/ErrorResponseDTO.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/exception/ErrorResponseDTO.java
@@ -1,34 +1,20 @@
 package edu.kangwon.university.taxicarpool.exception;
 
+import lombok.Getter;
 import java.time.LocalDateTime;
 
+@Getter
 public class ErrorResponseDTO {
 
-    private int status;
-    private String message;
-    private String path;
-    private LocalDateTime timestamp;
+    private final int status;
+    private final String message;
+    private final String path;
+    private final LocalDateTime timestamp;
 
     public ErrorResponseDTO(int status, String message, String path) {
         this.status = status;
         this.message = message;
         this.path = path;
         this.timestamp = LocalDateTime.now();
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
     }
 }
