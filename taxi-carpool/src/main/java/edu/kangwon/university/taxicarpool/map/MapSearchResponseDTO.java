5769d22c2ccad0ee0909ff459f4d6e54052da741
suno-boy <tnsgh3691@gmail.com>
2025-09-23 18:07:46 +0900

    refactor: map DTO 롬복 적용

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/map/MapSearchResponseDTO.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/map/MapSearchResponseDTO.java
@@ -1,16 +1,11 @@
 package edu.kangwon.university.taxicarpool.map;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class MapSearchResponseDTO {
-
     private final List<MapPlaceDTO> places;
-
-    public MapSearchResponseDTO(List<MapPlaceDTO> places) {
-        this.places = places;
-    }
-
-    public List<MapPlaceDTO> getPlaces() {
-        return places;
-    }
 }
