08998faff799909ed0803b4c706c023854493538
suno-boy <tnsgh3691@gmail.com>
2025-09-23 18:03:34 +0900

    refactor: member Entity / DTO 롬복 적용

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/party/dto/PartyUpdateRequestDTO.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/party/dto/PartyUpdateRequestDTO.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
