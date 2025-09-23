dcecb2f6cae54f0fec8828d62e17e33d244514d2
suno-boy <tnsgh3691@gmail.com>
2025-09-23 18:16:01 +0900

    refactor: 이메일 Entity, DTO 롬복 적용

--- taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/email/EmailVerificationEntity.java
+++ taxi-carpool/src/main/java/edu/kangwon/university/taxicarpool/email/EmailVerificationEntity.java
@@ -1,15 +1,16 @@
 package edu.kangwon.university.taxicarpool.email;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailVerificationEntity {
 
     @Id
@@ -29,9 +30,6 @@ public class EmailVerificationEntity {
     @NotNull
     private boolean verified;
 
-    public EmailVerificationEntity() {
-    }
-
     public EmailVerificationEntity(String email, String verificationCode, LocalDateTime expiresAt) {
         this.email = email;
         this.verificationCode = verificationCode;
@@ -43,35 +41,8 @@ public class EmailVerificationEntity {
         return LocalDateTime.now().isAfter(this.expiresAt);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public LocalDateTime getExpiresAt() {
-        return expiresAt;
-    }
-
-    public boolean isVerified() {
-        return verified;
-    }
-
     public void setVerified(boolean verified) {
         this.verified = verified;
     }
 
-    public void setExpiresAt(LocalDateTime expiresAt) {
-        this.expiresAt = expiresAt;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
 }
